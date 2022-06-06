fb4943b943dbbe76faac733aa8a32fb548d63779
Daniel <danielhunterteo@gmail.com>
2022-06-06 19:30:45 -0300

    atualização Heroku

--- pypro/base/models.py
+++ pypro/base/models.py
@@ -87,5 +87,5 @@ class User(AbstractBaseUser, PermissionsMixin):
         return self.first_name
 
     def email_user(self, subject, message, from_email=None, **kwargs):
-        """Send an email to this user."""
+        """envie um email para esse usuario."""
         send_mail(subject, message, from_email, [self.email], **kwargs)
